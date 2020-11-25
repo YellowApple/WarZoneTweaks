20d98d43a4a524964bc659ffd6651a7bcbe6fc35
Ryan S. Northrup (RyNo) <northrup@yellowapple.us>
2020-11-24 17:15:46 -0800

    Enable support for Avorion versions < 1.4

--- data/scripts/sector/background/warzonecheck.lua
+++ data/scripts/sector/background/warzonecheck.lua
@@ -2,6 +2,8 @@ if onServer() then
 
     function WarZoneCheck.restore(data_in)
         self.data = data_in
+        print("WarZoneCheck: original sector war zone data: ")
+        printTable(self.data)
 
         -- We override a couple data members that are supposed to be
         -- "constants", so that existing sectors get these tweaks, too.
@@ -9,6 +11,8 @@ if onServer() then
         self.data.warZoneThreshold = 75
         self.data.pacefulThreshold = 25 -- "paceful" ಠ_ಠ
         self.data.maxScore = 100
+        print("WarZoneCheck: adjusted sector war zone data: ")
+        printTable(self.data)
     end
 
     -- Common function to evaluate destruction/capture score increase.
@@ -42,6 +46,7 @@ if onServer() then
         end
         if delta == 0 then return end
 
+        print("WarZoneCheck: increasing war zone score by ", delta, " points")
         WarZoneCheck.increaseScore(delta)
     end
 
